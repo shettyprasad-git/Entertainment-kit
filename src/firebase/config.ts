851b68d98cb1d2dd5ce0e849fc78af530269a525
Prasad Shetty <prasadshetty1275@gmail.com>
2026-02-22 16:45:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8078325007-18445",
+  "appId": "1:988835854408:web:f3943b84fc22944a64c07d",
+  "apiKey": "AIzaSyCHOjCXJzCSbHukXWK_AYGfhwZlSArf8qc",
+  "authDomain": "studio-8078325007-18445.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "988835854408"
+};
