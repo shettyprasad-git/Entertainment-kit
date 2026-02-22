e982bfa0817ef3def8cf6d7cb57367d68422734f
Prasad Shetty <prasadshetty1275@gmail.com>
2026-02-22 17:10:46 +0000

    help me host this in vercel

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8078325007-18445",
-  "appId": "1:988835854408:web:f3943b84fc22944a64c07d",
-  "apiKey": "AIzaSyCHOjCXJzCSbHukXWK_AYGfhwZlSArf8qc",
-  "authDomain": "studio-8078325007-18445.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "988835854408"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
